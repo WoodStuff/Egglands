2fb1d68fec2ff75765e18c855c13807d73e1661f
TheEgglet <woodstuffthings@gmail.com>
2025-01-19 20:49:56 +0100

    finished Options method

--- Egglands/Control.cs
+++ Egglands/Control.cs
@@ -1,59 +1,74 @@
-﻿namespace Egglands
+﻿namespace Egglands;
+
+/// <summary>
+/// Contains methods for controlling the flow of the game.
+/// </summary>
+internal static class Control
 {
+	private const ConsoleColor UNSELECTED_OPTION = ConsoleColor.White;
+	private const ConsoleColor SELECTED_OPTION = ConsoleColor.Blue;
+
+	private static readonly ConsoleKey[] OptionConfirmKeys = [ConsoleKey.Enter, ConsoleKey.Spacebar];
+
 	/// <summary>
-	/// Contains methods for controlling the flow of the game.
+	/// Enforces a choice by waiting until one of some keys is pressed.
 	/// </summary>
-	internal static class Control
+	/// <param name="keys">The acceptable keys.</param>
+	/// <returns>The pressed key.</returns>
+	public static ConsoleKeyInfo Wait(ConsoleKey[] keys)
 	{
-		/// <summary>
-		/// Enforces a choice by waiting until one of some keys is pressed.
-		/// </summary>
-		/// <param name="keys">The acceptable keys.</param>
-		/// <returns>The pressed key.</returns>
-		public static ConsoleKeyInfo Wait(ConsoleKey[] keys)
+		ConsoleKeyInfo key;
+
+		do
 		{
-			ConsoleKeyInfo key;
+			key = Console.ReadKey(true);
+		}
+		while (!keys.Contains(key.Key)); // only proceed if requested key is pressed
 
-			do
-			{
-				key = Console.ReadKey(true);
-			}
-			while (!keys.Contains(key.Key)); // only proceed if requested key is pressed
+		return key;
+	}
 
-			return key;
+	/// <summary>
+	/// Enforces a choice by writing out some options and allowing the user to pick one using arrow keys and enter.
+	/// </summary>
+	/// <param name="options">The selectable options.</param>
+	/// <returns>The index of the option that was selected.</returns>
+	public static int Options(string[] options)
+	{
+		int start = Console.CursorTop; // position of the option list
+		int selected = 0; // currently selected option id
+
+		foreach (string option in options)
+		{
+			bool first = option == options[0];
+			Writing.Write(option, first ? SELECTED_OPTION : UNSELECTED_OPTION);
 		}
 
-		/// <summary>
-		/// Enforces a choice by writing out some options and allowing the user to pick one using arrow keys and enter.
-		/// </summary>
-		/// <param name="options">The selectable options.</param>
-		/// <returns>The index of the option that was selected.</returns>
-		public static int Options(string[] options)
+		ConsoleKeyInfo key;
+		do
 		{
-			//int start = Console.CursorTop; // position of the option list
-			int selected = 0; // currently selected option id
+			// up and down arrows for choosing options
+			// enter to confirm option
+			key = Wait([..OptionConfirmKeys, ConsoleKey.UpArrow, ConsoleKey.DownArrow]);
 
-			foreach (string option in options)
+			if (!OptionConfirmKeys.Contains(key.Key))
+				Writing.Write(options[selected], UNSELECTED_OPTION, (0, start + selected));
+
+			if (key.Key == ConsoleKey.UpArrow) // move selected option up
 			{
-				Console.WriteLine(option);
-			}
-
-			ConsoleKeyInfo key;
-			do
-			{
-				// up and down arrows for choosing options
-				// enter to confirm option
-				key = Wait([ConsoleKey.UpArrow, ConsoleKey.DownArrow, ConsoleKey.Enter]);
-
-				if (key.Key == ConsoleKey.UpArrow)   selected--; // move selected option up
-				if (key.Key == ConsoleKey.DownArrow) selected++; // move selected option down
-
-				// looping once selection goes off edge
+				selected--;
 				if (selected < 0) selected = options.Length - 1;
+			}
+			if (key.Key == ConsoleKey.DownArrow) // move selected option down
+			{
+				selected++;
 				if (selected > options.Length - 1) selected = 0;
-			} while (key.Key != ConsoleKey.Enter);
+			}
 
-			return selected;
-		}
+			if (!OptionConfirmKeys.Contains(key.Key))
+				Writing.Write(options[selected], SELECTED_OPTION, (0, start + selected));
+		} while (!OptionConfirmKeys.Contains(key.Key));
+
+		return selected;
 	}
 }
