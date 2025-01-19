db859c1fde34ac2b836c55496ea7ec5bbc4bada8
TheEgglet <woodstuffthings@gmail.com>
2025-01-19 15:56:28 +0100

    Add project files.

---
+++ Egglands/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Egglands
+{
+	internal class Program
+	{
+		private int option = 0;
+		static void Main()
+		{
+			Console.WriteLine("====================");
+			Console.WriteLine("      EGGLANDS      ");
+			Console.WriteLine("====================");
+		}
+	}
+}
