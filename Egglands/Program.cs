f432804e8c811eb28e5a5820e45903c142512d55
TheEgglet <woodstuffthings@gmail.com>
2025-01-19 20:58:38 +0100

    base menu structure

--- Egglands/Program.cs
+++ Egglands/Program.cs
@@ -1,14 +1,17 @@
-﻿namespace Egglands
-{
-	internal class Program
-	{
-		static void Main()
-		{
-			Console.WriteLine("====================");
-			Console.WriteLine("      EGGLANDS      ");
-			Console.WriteLine("====================");
+﻿namespace Egglands;
 
-			Control.Wait(['a', 'b']);
-		}
+internal class Program
+{
+	static void Main()
+	{
+		Console.CursorVisible = false;
+
+		Console.WriteLine("====================");
+		Console.WriteLine("      EGGLANDS      ");
+		Console.WriteLine("====================");
+		Console.WriteLine("");
+
+		var option = Control.Options(["Play", "Exit"]);
+		if (option == 1) Environment.Exit(0);
 	}
 }
