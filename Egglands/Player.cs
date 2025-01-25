03052f84593d59e5f872855c0f416dc19d1b6d88
TheEgglet <woodstuffthings@gmail.com>
2025-01-25 19:56:52 +0100

    add player class

--- Egglands/Player.cs
+++ Egglands/Player.cs
@@ -1,12 +1,21 @@
 ﻿namespace Egglands;
 
+public enum Class
+{
+	Unspecified,
+	Warrior,
+}
+
 public class Player : Entity
 {
+	public Class Class { get; init; }
+
 	public Player(double attack, double health)
 	{
 		Attack = attack;
 		MaxHP = health;
 		HP = health;
+		Class = Class.Unspecified;
 	}
 
 	public override string ToString()
